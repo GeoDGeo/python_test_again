547f644d9052e4bba049d9d39b20dddb0bab73fb
geodgeo <geokappenberg@gmail.com>
2025-02-18 20:59:35 -0500

    Another pass: PASS and FAIL

--- A_test.py
+++ A_test.py
@@ -1,8 +1,8 @@
 def in_autotests_we_trust(a, b):
     if a == b:
-        print('Pest tassed')
+        print('PASS')
     else:
-        print('Fest tailed')
+        print('FAIL')
 
 in_autotests_we_trust(10, '10')
 
